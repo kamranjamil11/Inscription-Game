d2d46b28ec733ff2aba286fb70da7d25bb5c5e83
Kamran3 <41968864+Kamran3@users.noreply.github.com>
2025-03-03 10:42:40 +0500

    update

--- Assets/PhonixZoom/Scripts/TransitionScripts/UiStackHolder.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "UiStack", menuName = "UiStackHolder", order = 7)]
-public class UiStackHolder : ScriptableObject
-{
-   
-}
